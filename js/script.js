9eb759926af01bba46ddecc89aad0b2cdc37a2d5
YejinSeo <47125879+YejinSeo@users.noreply.github.com>
2019-11-27 04:44:17 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-function 
+function check(e) {
+  if (first.value == "")
+  {
+    alert("Enter your first name");
+    e.preventDefault();
+    return false;
+  }
+  else  if (second.value == "")
+  {
+    alert("Enter your second name");
+    e.preventDefault();
+    return false;
+  }
+  else  if (email.value == "")
+  {
+    alert("Enter your email");
+    e.preventDefault();
+    return false;
+  }
+  else  if (phone.value == "")
+  {
+    alert("Enter your phone number");
+    e.preventDefault();
+    return false;
+  }
+  else
+  {
+    alert("Thank you for joining with us!");
+    return ture;
+  }
